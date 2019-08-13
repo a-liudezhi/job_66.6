eb9d7339d5bf3ae44178c5e4d0c834a516ef2a95
a-liudezhi <think72013@aliyun.com>
2019-08-13 10:43:59 +0800

    表栏错误提示方式

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,13 +14,14 @@
 #
 
 class Job < ApplicationRecord
-  validates :title, presence: true
-  validates :brand, presence: true
-  validates :description, presence: true
-  validates :wage_upper_bound, presence: true
-  validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: { greater_than: 0}
-  validates :location, presence: true
+  validates :title, presence: { message: "请填写职位名称" }
+  validates :brand, presence: { message: "请填写职位品牌/公司" }
+  validates :description, presence: { message: "请填写职位描述/要求" }
+  validates :wage_lower_bound, presence: { message: "请填写最低薪水" }
+  validates :wage_upper_bound, presence: { message: "请填写最高薪水" }
+  validates :wage_lower_bound, numericality: { greater_than: 0, message: "最低薪水必须大于零" }
+  validates :wage_upper_bound, numericality: { greater_than: :wage_lower_bound, message: "薪水上限不能低于薪水下限" }
+  validates :location, presence: { message: "请填写工作地点" }
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
   has_many :resumes, dependent: :destroy
