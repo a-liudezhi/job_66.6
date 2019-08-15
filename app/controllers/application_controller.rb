669114d8db1af8543fd3a9baf7c60072993d521e
a-liudezhi <think72013@aliyun.com>
2019-08-16 00:06:15 +0800

    可以修改帐户名字

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,22 @@ def set_locale  #設定多國語系
 
   I18n.locale = session[:locale] || I18n.default_locale
  end
+
+   protected
+
+   before_action :configure_permitted_parameters, if: :devise_controller?
+   before_action :configure_permitted, if: :devise_controller?
+
+   def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up) do |u|
+       u.permit(:name, :email, :password, :password_confirmation)
+     end
+    end
+
+    def configure_permitted
+     devise_parameter_sanitizer.permit(:account_update) do |u|
+     u.permit(:name, :email, :password, :password_confirmation, :current_password)
+   end
+ end
+ 
 end
