acbecca427a25fc44966503d492ef2e2a6671f76
liudezhi <a.liudezhi@gmail.com>
2019-08-22 00:24:12 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if User.find_by(email: "a.liudezhi@gmail.com").nil?
+u = User.new
+u.email = "a.liudezhi@gmail.com"           # 可以改成自己的 email
+
+u.password = "88888888"                # 最少要六码
+
+u.password_confirmation = "88888888"   # 最少要六码
+
+u.is_admin = true
+u.save
+puts "Admin 已经建立好了，帐号为#{u.email}, 密码为#{u.password}"
+else
+puts "Admin 已经建立过了，脚本跳过该步骤。"
+end
